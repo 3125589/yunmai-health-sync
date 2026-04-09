a26986ad5856b54c84e19cc2cc7c3e0693f01107
wangdi <157371567@qq.com>
2026-04-09 23:39:39 +0800

    Fix: simplify - remove metadata/device, use default constructor

--- android-app/app/src/main/java/com/yunmai/healthsync/HealthConnectHelper.kt
+++ android-app/app/src/main/java/com/yunmai/healthsync/HealthConnectHelper.kt
@@ -5,7 +5,6 @@ import android.util.Log
 import androidx.health.connect.client.HealthConnectClient
 import androidx.health.connect.client.records.BodyFatRecord
 import androidx.health.connect.client.records.WeightRecord
-import androidx.health.connect.client.records.metadata.Device
 import androidx.health.connect.client.units.Mass
 import androidx.health.connect.client.units.Percentage
 import java.time.Instant
@@ -34,10 +33,7 @@ class HealthConnectHelper(context: Context) {
             val weightRecord = WeightRecord(
                 time = instant,
                 zoneOffset = zoneOffset,
-                weight = Mass.kilograms(weight),
-                metadata = androidx.health.connect.client.records.metadata.Metadata.autoRecorded(
-                    device = Device()
-                )
+                weight = Mass.kilograms(weight)
             )
 
             healthConnectClient.insertRecords(listOf(weightRecord))
@@ -60,10 +56,7 @@ class HealthConnectHelper(context: Context) {
             val bodyFatRecord = BodyFatRecord(
                 time = instant,
                 zoneOffset = zoneOffset,
-                percentage = Percentage(fat),
-                metadata = androidx.health.connect.client.records.metadata.Metadata.autoRecorded(
-                    device = Device()
-                )
+                percentage = Percentage(fat)
             )
 
             healthConnectClient.insertRecords(listOf(bodyFatRecord))
